323b13d04be2c17421b5b5308378156909f44006
Gopika <kotakalagopika@gmail.com>
2025-04-17 22:03:24 +0530

    first commit of frontend and backend

---
+++ Backend/Movie/src/main/java/com/example/Movie/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.example.Movie.repository;
+
+import com.example.Movie.model.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
